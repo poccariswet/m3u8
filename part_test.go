df641e8868fbe24a5e5228e37966d86cff3db4ad
poccariswet <s1240231@gmail.com>
2019-06-24 23:58:01 +0900

    [ADD] part inf and part test

--- part_test.go
+++ part_test.go
@@ -1,1 +1,34 @@
 package m3u8_test
+
+import (
+	"testing"
+
+	m3u8 "github.com/poccariswet/m3u8-decoder"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPartInf(t *testing.T) {
+	line := `#EXT-X-PART-INF:PART-TARGET=0.33334`
+
+	pi, err := m3u8.NewPartInf(line)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	assert.Equal(t, float64(0.33334), pi.PartTartget)
+}
+
+func TestPart(t *testing.T) {
+	line := `#EXT-X-PART:DURATION=0.33334,URI="filePart272.f.ts",INDEPENDENT=YES,GAP=YES`
+
+	part, err := m3u8.NewPart(line)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	assert.Equal(t, float64(0.33334), part.Duration)
+	assert.Equal(t, "filePart272.f.ts", part.URI)
+	assert.True(t, part.Independent)
+	assert.Nil(t, part.ByteRange)
+	assert.True(t, part.Gap)
+}
