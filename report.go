50250339c453a931bcb6d43c182e5353cb8c1b07
poccariswet <s1240231@gmail.com>
2019-07-02 00:47:54 +0900

    [ADD] decode rendition report

--- report.go
+++ report.go
@@ -1,15 +1,48 @@
 package m3u8
 
+import (
+	"fmt"
+	"strings"
+
+	"github.com/pkg/errors"
+)
+
 type RenditionReportSegment struct {
 	URI      string
-	LastMSN  int
-	LastPART int
+	LastMSN  uint64
+	LastPART uint64
 }
 
 func NewReport(line string) (*RenditionReportSegment, error) {
-	return &RenditionReportSegment{}, nil
+	item := parseLine(line[len(ExtRenditionReport+":"):])
+
+	lastMSN, err := extractUint64(item, LASTMSN)
+	if err != nil {
+		return nil, errors.Wrap(err, "extractUint64 err")
+	}
+
+	lastPART, err := extractUint64(item, LASTPART)
+	if err != nil {
+		return nil, errors.Wrap(err, "extractUint64 err")
+	}
+	return &RenditionReportSegment{
+		URI:      item[URI],
+		LastMSN:  lastMSN,
+		LastPART: lastPART,
+	}, nil
 }
 
 func (rs *RenditionReportSegment) String() string {
-	return "RenditionReportSegment"
+	var s []string
+
+	s = append(s, fmt.Sprintf(`%s="%s"`, URI, rs.URI))
+
+	if rs.LastMSN != 0 {
+		s = append(s, fmt.Sprintf("%s=%d", LASTMSN, rs.LastMSN))
+	}
+
+	if rs.LastPART != 0 {
+		s = append(s, fmt.Sprintf("%s=%d", LASTPART, rs.LastPART))
+	}
+	return fmt.Sprintf("%s:%s", ExtRenditionReport, strings.Join(s, ","))
 }
